e199417a95f1f8c2f4309739791b19ba5cbb9f31
akashkalita987 <akashjyotikalita76@gmail.com>
2026-03-23 17:24:34 +0530

    adding new things

--- pattern.cpp
+++ pattern.cpp
@@ -8,8 +8,32 @@ void print1(int n){
         cout<<endl;
     }
 }
+void print2(int n){
+    for(int i = 1; i<=n; i++){
+        for(int j=1; j<=i; j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
+void print3(int n){
+    for (int i=1; i<=n; i++){
+        for(int j=1; j<=i; j++){
+            cout<<j;
+        }
+        cout<<endl;
+    }
+}
+void print4(int n){
+    for(int i=1; i<=n;i++){
+        for(int j=1;j<=i;j++){
+            cout<<i;
+        }
+        cout<<endl;
+    }
+}
 int main(){
     int n = 5;
-    print1(n);
+    print4(n);
     return 0;
 }
