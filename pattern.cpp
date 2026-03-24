8aaf682b8e674821a8c2f13b2ace8a83ea580497
akashkalita987 <akashjyotikalita76@gmail.com>
2026-03-24 21:19:45 +0530

    print9

--- pattern.cpp
+++ pattern.cpp
@@ -82,8 +82,18 @@ void print8(int n){
         cout<<endl;
     }
 }
+void print9(int n){
+    for(int i=1; i<=2*n-1; i++){
+        int stars = i;
+        if(i>n) stars = 2*n - i;
+        for(int j=1; j<=stars; j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
 int main(){
     int n = 5;
-    print8(n);
+    print9(n);
     return 0;
 }
