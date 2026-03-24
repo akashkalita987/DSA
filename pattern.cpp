b4e14729e02b05ee65c5185b112fa5b814130011
akashkalita987 <akashjyotikalita76@gmail.com>
2026-03-24 21:29:38 +0530

    new pattern

--- pattern.cpp
+++ pattern.cpp
@@ -92,8 +92,20 @@ void print9(int n){
         cout<<endl;
     }
 }
+void print10(int n){
+    int start = 1;
+    for(int i=0; i<n; i++){
+        if(i % 2==0)start = 1;
+        else start = 0;
+        for(int j=0; j<=i; j++){
+            cout<<start;
+            start = 1-start;
+        }
+        cout<<endl;
+    }
+}
 int main(){
     int n = 5;
-    print9(n);
+    print10(n);
     return 0;
 }
