3e24b4675344042a755414a5a316f4cf67ae6456
akashkalita987 <akashjyotikalita76@gmail.com>
2026-03-24 19:57:22 +0530

    adding new pattern

--- pattern.cpp
+++ pattern.cpp
@@ -49,10 +49,24 @@ void print6(int n){
     }
 }
 void print7(int n){
-    
+    for(int i=0; i<n; i++){
+    //space
+        for( int j=0; j<n-i-1; j++){
+            cout<<" ";
+    }
+    //stars
+        for( int j=0; j< 2*i+1; j++ ){
+            cout<<"*";
+    }
+    //space
+        for( int j=0; j<n-i-1; j++){
+            cout<<" ";
+    }
+    cout<<endl;
+    }
 }
 int main(){
     int n = 5;
-    print6(n);
+    print7(n);
     return 0;
 }
