5ddbf693ee085144e6cf7b61ba5b5d488feb171a
akashkalita987 <akashjyotikalita76@gmail.com>
2026-03-24 21:04:58 +0530

    addded another thing

--- pattern.cpp
+++ pattern.cpp
@@ -65,8 +65,25 @@ void print7(int n){
     cout<<endl;
     }
 }
+void print8(int n){
+    for(int i=0;i<n;i++){
+        //space
+        for(int j=0; j<i; j++){
+            cout<<" ";
+        }
+        //stars
+        for(int j=0; j<2*n-(2*i+1); j++){
+            cout<<"*";
+        }
+        //space
+        for(int j=0; j<i; j++){
+            cout<<" ";
+        }
+        cout<<endl;
+    }
+}
 int main(){
     int n = 5;
-    print7(n);
+    print8(n);
     return 0;
 }
