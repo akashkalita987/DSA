1176874e716e4c488aa2e0155776a9b871ad8461
akashkalita987 <akashjyotikalita76@gmail.com>
2026-03-24 07:12:51 +0530

    adding new things

--- pattern.cpp
+++ pattern.cpp
@@ -1,8 +1,8 @@
 #include<iostream>
 using namespace std;
 void print1(int n){
-    for(int i = 0; i<n; i++){
-        for(int j = 0; j<n; j++){
+    for(int i = 0; i<=n; i++){
+        for(int j = 0; j<=n; j++){
             cout<<"*";
         }
         cout<<endl;
@@ -32,8 +32,27 @@ void print4(int n){
         cout<<endl;
     }
 }
+void print5(int n){
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=n-i+1; j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
+void print6(int n){
+    for(int i=1; i<=n; i++){
+        for( int j=1; j<=n-i+1; j++){
+            cout<<j;
+        }
+        cout<<endl;
+    }
+}
+void print7(int n){
+    
+}
 int main(){
     int n = 5;
-    print4(n);
+    print6(n);
     return 0;
 }
