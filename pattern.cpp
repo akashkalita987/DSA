220f72bc3224d6121979c8bad116a74a2fd8160a
akashkalita987 <akashjyotikalita76@gmail.com>
2026-03-26 20:47:49 +0530

    add

--- pattern.cpp
+++ pattern.cpp
@@ -133,8 +133,18 @@ void print12(int n){
         cout<<endl;
     }
 }
+void print13(int n){
+    char  ch ='A';
+    for(int i =1; i<=n; i++){
+        for(int j=1; j<=i; j++){
+            cout<<ch<<" ";
+            ch =ch +1;
+    }
+    cout<<endl;
+    }
+}
 int main(){
     int n = 5;
-    print12(n);
+    print13(n);
     return 0;
 }
