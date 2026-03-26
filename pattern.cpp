1c367322d4d519564299fcd7633a774b8ae88f1a
akashkalita987 <akashjyotikalita76@gmail.com>
2026-03-26 21:03:33 +0530

    added new things

--- pattern.cpp
+++ pattern.cpp
@@ -143,8 +143,17 @@ void print13(int n){
     cout<<endl;
     }
 }
+void print14(int n){
+    char ch='A';
+    for(int i=0; i<n; i++){
+        for (char ch ='A'; ch<='A'+i; ch++){
+            cout<<ch<<" ";
+        }
+        cout<<endl;
+    }
+}
 int main(){
     int n = 5;
-    print13(n);
+    print14(n);
     return 0;
 }
