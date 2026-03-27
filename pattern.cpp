911ad1a19d39ae95c2b9878185997a81fe8a2d39
akashkalita987 <akashjyotikalita76@gmail.com>
2026-03-27 11:22:20 +0530

    add

--- pattern.cpp
+++ pattern.cpp
@@ -191,8 +191,52 @@ void print17(int n){
         cout<<endl;
     }
 }
+void print18(int n){
+    for(int i=0;i<n;i++){
+        for(char ch='E'-i; ch<='E'; ch++){
+            cout<<ch<<" ";
+        }
+        cout<<endl;
+    }
+}
+void print19(int n){
+    int iniS=0;
+    for(int i=0;i<n;i++){
+        //stars
+        for(int j=1;j<=n-i;j++){
+            cout<<"*";
+        }
+        //spaces
+        for(int j =0;j<iniS;j++){
+            cout<<" ";
+        }
+        //stars
+        for(int j=1; j<=n-i;j++){
+            cout<<"*";
+        }
+        iniS+=2;
+        cout<<endl;
+    }
+    iniS=8;
+    for(int i=1;i<=n;i++){
+        //stars
+        for(int j=1;j<=i;j++){
+            cout<<"*";
+        }
+        //spaces
+        for(int j =0;j<iniS;j++){
+            cout<<" ";
+        }
+        //stars
+        for(int j=1; j<=i;j++){
+            cout<<"*";
+        }
+        iniS-=2;
+        cout<<endl;
+    }
+}
 int main(){
     int n = 5;
-    print17(n);
+    print19(n);
     return 0;
 }
