082db97a005b7d7cbb31e2b83b417c47148d5a3b
akashkalita987 <akashjyotikalita76@gmail.com>
2026-03-28 22:10:51 +0530

    Pair update

--- pair.cpp
+++ pair.cpp
@@ -1,11 +1,31 @@
 #include<iostream>
 using namespace std;
-void explainPair(){
+//single pair 
+
+void singlePair(){
     pair<int, int> p = {1,3};
     cout<<p.first<<" "<<p.second;
+    cout<<endl;
+}
+
+//nested pair
+
+void nesPair(){
+    pair<int, pair<int, int>> p = {1, { 2 , 3 } };
+    cout<<p.first<<" "<< p.second.second<<" "<<p.second.first;
+    cout<<endl;
+}
+
+//array pair
+
+void arrPair(){
+    pair<int, int> arr[] = { {1,2}, {3,4}, {5,6}};
+    cout<<arr[1].second;
 }
 int main(){
-    explainPair();
+    singlePair();
+    nesPair();
+    arrPair();
     return 0;
    
 }
