966709e466da8da4f312a0fd4e4a26f4d4357c94
akashkalita987 <akashjyotikalita76@gmail.com>
2026-03-28 16:24:08 +0530

    addding

--- pattern.cpp
+++ pattern.cpp
@@ -257,8 +257,19 @@ void print20(int n){
         else spaces += 2;
     }
 }
+void print21(int n){
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            if(i==0 || i== n-1 ||j==0 || j==n-1){
+                cout<<"*";
+            }
+            else cout<<" ";
+        }
+        cout<<endl;
+    }
+}
 int main(){
     int n = 5;
-    print20(n);
+    print21(n);
     return 0;
 }
