8889377700a295f1144e8ec9d220ce651bbd2d0b
akashkalita987 <akashjyotikalita76@gmail.com>
2026-03-31 20:28:19 +0530

    added new things

--- queue.cpp
+++ queue.cpp
@@ -1,6 +1,21 @@
 #include<iostream>
 #include<queue>
 using namespace std;
+
+void explainqueue(){
+    priority_queue<int>pq;
+
+    pq.push(5);//{2}
+    pq.push(2);//{5,2}
+    pq.push(8);//{8,5,2}
+    pq.emplace(10);//{10,8,5,2}
+
+    cout<<pq.top(); //{8, 5, 2}
+
+    pq.pop(); //{8, 5, 2}
+
+    cout<<pq.top(); //print 8
+}
 int main(){
-    
+
 }
