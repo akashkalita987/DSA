3611d8789fa88e47a47826dd4fd350a19be3b787
akashkalita987 <akashjyotikalita76@gmail.com>
2026-03-31 20:48:32 +0530

    added

--- queue.cpp
+++ queue.cpp
@@ -5,27 +5,30 @@ using namespace std;
 void explainqueue(){
     priority_queue<int>pq;
 
-    pq.push(5);//{2}
-    pq.push(2);//{5,2}
-    pq.push(8);//{8,5,2}
-    pq.emplace(10);//{10,8,5,2}
+    pq.push(5);
+    pq.push(2);
+    pq.push(8);
+    pq.emplace(10);
 
-    cout<<pq.top(); //{8, 5, 2}
-
-    pq.pop(); //{8, 5, 2}
-
-    cout<<pq.top(); //print 8
-
-    //size swap empty function same as others
-    //minimum heap
+    cout << "Max Heap (pq): ";
+    while(!pq.empty()) {
+        cout << pq.top() << " ";
+        pq.pop();
+    }
+    cout << endl;
 
     priority_queue<int, vector<int>>pg;
-    pg.push(5);//{2}
-    pg.push(2);//{2,5}
-    pg.push(8);//{2,5,8}
-    pg.emplace(10);//2,5,8,10}
+    pg.push(5);
+    pg.push(2);
+    pg.push(8);
+    pg.emplace(10);
 
-    cout<<pg.top(); //print 2
+    cout << "Min Heap (pg): ";
+    while(!pg.empty()) {
+        cout << pg.top() << " ";
+        pg.pop();
+    }
+    cout << endl;
 }
 int main(){
     explainqueue();
