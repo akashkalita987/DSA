e252684b6888735f5308be233e7a03416ed56495
akashkalita987 <akashjyotikalita76@gmail.com>
2026-04-02 22:12:09 +0530

    add

--- set.cpp
+++ set.cpp
@@ -7,8 +7,18 @@ void explainSet(){
     st.insert(7);//{5,7}
     st.insert(3);//{5,7,3}
     st.insert(4);//{5,7,3,4}
-    st.insert(9);//{5,7,3,4}
-    st.emplace(5);//{5,7,3,4,5}
+    st.insert(9);//{5,7,3,4,9}
+    st.emplace(5);//{5,7,3,4,9,5}
+
+    //{5,7,3,4,9,5}
+    auto it = st.find(3);  
+    
+    //{5,7,3,4,9,5}
+    auto it = st.find(6);  
+
+    st.erase(5);
+
+    int cnt = st.count(1);
 }
 int main(){
     
