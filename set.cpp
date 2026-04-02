b23b5e9af22669bcca6e03e289f356b27ad61352
akashkalita987 <akashjyotikalita76@gmail.com>
2026-04-02 22:06:07 +0530

    adding values to set

--- set.cpp
+++ set.cpp
@@ -1,6 +1,15 @@
 #include<iostream>
 #include<set>
 using namespace std;
+void explainSet(){
+    set<int>st;
+    st.insert(5);//{5}
+    st.insert(7);//{5,7}
+    st.insert(3);//{5,7,3}
+    st.insert(4);//{5,7,3,4}
+    st.insert(9);//{5,7,3,4}
+    st.emplace(5);//{5,7,3,4,5}
+}
 int main(){
     
     return 0;
