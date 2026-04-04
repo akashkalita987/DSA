a7bb34a418f817772e941413898cd44ddca69685
akashkalita987 <akashjyotikalita76@gmail.com>
2026-04-04 21:12:30 +0530

    complited

--- lower_bound.cpp
+++ lower_bound.cpp
@@ -1,11 +1,15 @@
-#include<iostream>
+#include <algorithm>
+#include <iostream>
 using namespace std;
 int main(){
-    int a[]  = {1,2,3,4,5,6};
+    int a[] = {1,2,3,4,5,6};
+    int n = sizeof(a) / sizeof(a[0]);
     int X;
-    int n = a.size();
-    cout<<"\nEnter the value of X:";
-    cin>>X;
-    int ind = lower_bound(a, a+n, X) - a;
 
+    cout << "\nEnter the value of X: ";
+    cin >> X;
+    int ind = lower_bound(a, a + n, X) - a;
+
+    cout << "Lower bound index = " << ind << "\n";
+    return 0;
 }
