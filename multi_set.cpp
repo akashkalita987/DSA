9469ddf173b4df589e26a4ee44bc9761ed027c90
akashkalita987 <akashjyotikalita76@gmail.com>
2026-04-07 21:44:08 +0530

    .

--- multi_set.cpp
+++ multi_set.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<set>
+#include<algorithm>
 using namespace std;
 
 void explainMulti(){
@@ -12,6 +13,12 @@ void explainMulti(){
     ms.insert(1); //{1,1,1}
 
     ms.erase(1); //all 1's ersaed
-
+    
     int cnt = ms.count(1);
+    
+    //only a single one erased
+    ms.erase(ms.find(1));
+    
+    ms.erase(ms.find(1), ms.find(1)+2);
+    //rest all function same as set
 }
