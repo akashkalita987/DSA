2a104eb5730622e2d5ee3c9a3f478eb38987ee0b
akashkalita987 <akashjyotikalita76@gmail.com>
2026-04-07 21:37:57 +0530

    eding things to multi set file

--- multi_set.cpp
+++ multi_set.cpp
@@ -3,5 +3,15 @@
 using namespace std;
 
 void explainMulti(){
-    
+    //Every thing is same set
+    //only stores duplicate elements also
+
+    multiset<int>ms;
+    ms.insert(1); //{1}
+    ms.insert(1); //{1,1}
+    ms.insert(1); //{1,1,1}
+
+    ms.erase(1); //all 1's ersaed
+
+    int cnt = ms.count(1);
 }
