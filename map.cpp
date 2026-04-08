7de77bc276142b3216bd6300146f08d5376afa08
akashkalita987 <akashjyotikalita76@gmail.com>
2026-04-08 20:31:37 +0530

    updated

--- map.cpp
+++ map.cpp
@@ -5,7 +5,7 @@ using namespace std;
 void explainMap(){
     map<int, int>mpp;
     map<int, pair<int, int>>mpp;
-    map<pair<int, int>, int>map;
+    map<pair<int, int>, int>mpps;
 
     //inserting 2 in mpp in 1 key
     mpp[1] = 2;
@@ -14,7 +14,17 @@ void explainMap(){
 
     mpp.insert({2,4});
 
-    map[{2,3}] = 10;
+    mpps[{2,3}] = 10;
 
-    for
+    for(auto it:mpp){
+        cout<<it.first<<" "<<it.second<<endl;
+    }
+
+    cout<<mpp[1];
+    cout<<mpp[5];
+
+    auto it = mpp.find(3);
+    cout<<(*it).second;
+
+    auto it = mpp.find(5);
 }
