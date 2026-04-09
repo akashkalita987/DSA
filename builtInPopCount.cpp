313666cfc6cae05676fdb70fc85f7119431863a7
akashkalita987 <akashjyotikalita76@gmail.com>
2026-04-09 21:38:22 +0530

    added a long number

--- builtInPopCount.cpp
+++ builtInPopCount.cpp
@@ -3,8 +3,12 @@
 using namespace std;
 
 int main (){
-    int num = 15;
-    int cnt = __builtin_popcount(num);
-    cout<<"The count is"<<cnt<<endl;
-    return 0;
+    int num1 = 15;
+    int cnt1 = __builtin_popcount(num1);
+    cout<<"The count is"<<cnt1<<endl;
+
+    long long num2=87236896178734;
+    int cnt2 = __builtin_popcount(num2);
+    cout<<"The count is"<<cnt2<<endl;
+     return 0;
 }
