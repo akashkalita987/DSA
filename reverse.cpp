a7c854eddf383cccaf4e8c68c08119439ae2c5f5
akashkalita987 <akashjyotikalita76@gmail.com>
2026-04-10 22:29:00 +0530

    reversed the number

--- reverse.cpp
+++ reverse.cpp
@@ -1,6 +1,18 @@
 #include<iostream>
 using namespace std;
-
-int main(){
+int revNo(int n){
+    int re = 0;
+    while (n>0)
+    {
+        int lastDigit = n % 10;
+        n = n/10;
+        re = (re*10)+lastDigit;
+    }
+    return re;
     
+}
+int main(){
+    int n = 95372;
+    cout<<"\nThe reverse is:"<<revNo(n);
+    return 0;
 }
