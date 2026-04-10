54582c1145cd103f51329fcc9aa889d00a94f2dd
akashkalita987 <akashjyotikalita76@gmail.com>
2026-04-10 23:11:01 +0530

    armstrong number

---
+++ armstrongNum.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"\nEnter the number";
+    cin>>n;
+    int original = n;
+    int digitCount = 0;
+    int temp = n;
+    if(n == 0){
+        digitCount = 1;
+    }else{
+        while(temp > 0){
+            digitCount++;
+            temp /= 10;
+        }
+    }
+    int sum = 0;
+    while(n > 0){
+        int lastDigit = n % 10;
+        sum += (int)pow(lastDigit, digitCount);
+        n /= 10;
+    }
+    if(sum == original){
+        cout << "Armstrong number" << endl;
+    }else{
+        cout << "Not an Armstrong number" << endl;
+    }
+    return 0;
+}
