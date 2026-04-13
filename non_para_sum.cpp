18fd5fe68433200827cb777f2b1f5620e2fe8309
akashkalita987 <akashjyotikalita76@gmail.com>
2026-04-13 20:48:51 +0530

    print sum till n number

---
+++ non_para_sum.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int sum(int n){
+    if(n==0) {return 0;}
+    else return n+sum(n-1);
+}
+
+int main(){
+    int n;
+    cout<<"\nEnter  the number till you want to sum:";
+    cin>>n;
+    cout<<sum(n);
+    return 0;
+}
