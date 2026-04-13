14e03f94812b9b0ec5f5cc27e7e1e99193b910a8
akashkalita987 <akashjyotikalita76@gmail.com>
2026-04-13 20:00:51 +0530

    doing sum of n numbers using recurtion

---
+++ sum_n_num.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+void sm(int i, int sum){
+    if(i<1) {
+        cout<<sum<<endl;;
+        return;
+    }
+    sm(i-1,sum+i);
+}
+
+int main(){
+    int n;
+    cout<<"\nEnter the name:";
+    cin>>n;
+    sm(n,0);
+    return 0;
+}
