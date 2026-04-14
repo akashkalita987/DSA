ea2af2702d13e94ae26a39e68da114d9a958701e
akashkalita987 <akashjyotikalita76@gmail.com>
2026-04-14 22:09:42 +0530

    changed the n

--- permutation.cpp
+++ permutation.cpp
@@ -4,9 +4,9 @@
 using namespace std;
 
 int main(){
-    string s = "7138";
+    string s = "342";
     sort(s.begin(),s.end());
-    int n = sizeof(s)/sizeof(s[0]);
+    int n = s.size();
     do{
         cout<<s<<endl;
     } while (next_permutation(s.begin(), s.end()));
