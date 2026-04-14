d35a9c5ecfdefd9d2942ec65360681ab03be68e5
akashkalita987 <akashjyotikalita76@gmail.com>
2026-04-14 21:46:12 +0530

    updated

--- fibonacci_recurtion.cpp
+++ fibonacci_recurtion.cpp
@@ -16,13 +16,14 @@ int fibo(int n){
 
 int main(){
     int n;
-    cout << "\nEnter the number of terms: ";
+    /*cout << "\nEnter the number of terms: ";
     cin >> n;
-
+    */
     // To print the series, we loop and call the function for each index
-    for(int i = 0; i < n; i++) {
+   /* for(int i = 0; i < n; i++) {
         cout << fibo(i) << (i == n - 1 ? "" : ", ");
-    }
+    }*/
+   cout<<fibo(4);
     
     return 0;
 }
