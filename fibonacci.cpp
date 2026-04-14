da5d852a2d1ffcc2aeac444dd28d1277622442f6
akashkalita987 <akashjyotikalita76@gmail.com>
2026-04-14 21:01:37 +0530

    finonacci without recurtion

---
+++ fibonacci.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int  i, n; 
+    long long t1=0, t2=1, nextTerm=0;
+    cout<<"\nEnter the limit";
+    cin>>n;
+
+    for( i =1; i<=n; ++i){
+        if(i==1){
+            cout<<t1<<",";
+            continue;
+        }
+        if(i==2){
+            cout<<t2<<",";
+            continue;
+        }
+        nextTerm= t1+t2;
+        t1=t2;
+        t2=nextTerm;
+    cout<<nextTerm<<(i==n ? "" :  ",");
+    }
+    cout<<endl;
+    return 0; 
+}
