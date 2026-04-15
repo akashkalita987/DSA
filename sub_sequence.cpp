5ef76ee1a9aca38cce9a3ad3725646176f313f63
akashkalita987 <akashjyotikalita76@gmail.com>
2026-04-15 21:59:39 +0530

    .

--- sub_sequence.cpp
+++ sub_sequence.cpp
@@ -3,11 +3,20 @@
 using namespace std;
 
 void print(int ind, vector<int>&ds, int arr[], int n){
-    
+    if(ind == n){
+        for(auto it:ds){
+            cout<<it<<" ";
+        }
+        cout<<endl;
+        return;
+    }
+
+    ds.push_back(arr[ind]);
 }
 int main(){
     int arr[] = {3,1,2};
     int n = 3;
     vector<int>ds;
+    print(0, ds, arr, n);
     return 0;
 }
