4146c06e44ad9246a3d0d63fbd49f9c8120eb059
akashkalita987 <akashjyotikalita76@gmail.com>
2026-04-18 22:28:37 +0530

    .

--- print.cpp
+++ print.cpp
@@ -2,6 +2,7 @@
 #include<algorithm>
 #include<set>
 #include<vector>
+#include<list>
 using namespace std;
 void vec(){
     vector<int>p = {1,2,3,4,5,6,7,8};
@@ -21,7 +22,32 @@ void pAir(){
     pair<string, string>p = {"hello","world"};
     cout<<p.first<<" "<<p.second<<endl;
 }
+
+void liSt(){
+    list<int>l = {1,2,3,4,5,6,7,8};
+    for(auto it : l){
+        cout<<it<<endl;
+    }
+}
 int main(){
-    pAir();
+    int choice;
+    cout<<"\nEnter the you want to print \n1.Vector\n2.Set\n3.Pair\n4.List"<<endl;
+    cin>>choice;
+    switch (choice)
+    {
+    case 1:
+        vec();
+        break;
+    case 2:
+        sEt();
+        break;
+    case 3:
+        pAir();
+        break;
+
+        case 4:
+        liSt();
+        break;
+    }
     return 0;
 }
