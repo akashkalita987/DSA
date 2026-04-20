d8bd6fec117b4d8fa9079cb68ecf167ce5ea7f7b
akashkalita987 <akashjyotikalita76@gmail.com>
2026-04-20 18:30:35 +0530

    printing sub sequance of anarray

--- sub_seq.cpp
+++ sub_seq.cpp
@@ -2,6 +2,7 @@
 #include<vector>
 #include<algorithm>
 using namespace std;
+//function
 void print(int ind, vector<int>&ds, int arr[], int n){
     if(ind==n){
         for(auto it: ds){
@@ -18,7 +19,7 @@ void print(int ind, vector<int>&ds, int arr[], int n){
     ds.pop_back();
     print(ind+1,ds,arr,n);
 }
-
+//main function
 int main(){
     
     int arr[] = {1,2,3,4,5,6,7,8,9};
