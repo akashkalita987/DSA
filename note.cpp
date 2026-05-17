16a7cf2aa8ccaf2c8ae2d399dcfc6c807f450f07
akashkalita987 <akashjyotikalita76@gmail.com>
2026-05-17 20:50:05 +0530

    .

--- note.cpp
+++ note.cpp
@@ -5,6 +5,13 @@ int main(){
     cout<<"\nEnter the limit";
     cin>>n;
 
-    
+    int a=0, b=1;
+    for(int i = 0; i<n; i++){
+        cout<<a<<" ";
+        int next = a+b;
+        a = b;
+        b = next;
+    }
+    cout<<endl;
     return 0;
 }
