b33b44cfa2d384379e65b97f9b80a7fa141c6976
akashkalita987 <akashjyotikalita76@gmail.com>
2026-05-17 20:42:00 +0530

    updated

--- fibo.cpp
+++ fibo.cpp
@@ -7,7 +7,7 @@ int main() {
     cin >> n;
 
     int a = 0, b = 1;
-    for(int i = 0; i < n; ++i) {
+    for(int i = 0; i < n; i++) {
         cout << a << " ";
         int next = a + b;
         a = b;
