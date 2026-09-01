e549f2ed2e9d1d38540ff185169beae1accde4f2
akashkalita987 <akashjyotikalita76@gmail.com>
2026-09-01 20:09:53 +0530

    display function

--- 01_Basics/pracctice.cpp
+++ 01_Basics/pracctice.cpp
@@ -1,29 +1,38 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-class SinglyLinked {
-    struct Node{
+class SinglyLinked
+{
+    struct Node
+    {
         int data;
-        Node* next;
-        Node (int val) : data(val), next(nullptr) {}
+        Node *next;
+        Node(int val) : data(val), next(nullptr) {}
     };
-    Node* head;
-    public:
-        SinglyLinked () : head(nullptr) {}
+    Node *head;
 
-    void insertAtHead(int val){
-        Node* newNode = new Node(val);
+public:
+    SinglyLinked() : head(nullptr) {}
+
+    void insertAtHead(int val)
+    {
+        Node *newNode = new Node(val);
         newNode->next = head;
         head = newNode;
     }
 
-    void insertAtEnd(int val){
-        Node* newNode = new Node(val);
-        if(head == nullptr) { head = newNode; return; }
-        Node* temp = head;
+    void insertAtEnd(int val)
+    {
+        Node *newNode = new Node(val);
+        if (head == nullptr)
+        {
+            head = newNode;
+            return;
+        }
+        Node *temp = head;
         while (temp->next != nullptr)
         {
-            temp = temp -> next;
+            temp = temp->next;
         }
         temp->next = newNode;
     }
@@ -47,5 +56,16 @@ class SinglyLinked {
             curr->next = curr->next->next;
             delete toDelete;
         }
+    }
+
+    void display()
+    {
+        Node *temp = head;
+        while (temp != nullptr)
+        {
+            cout << temp->data << " -> ";
+            temp = temp->next;
+        }
+        cout << "NULL" << endl;
     }
 };
